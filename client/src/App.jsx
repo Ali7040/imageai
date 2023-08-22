3a84fcc4f86a9db14e5a0dad3cb3d9ac6b6b73fc
Ali Haider <ha4219466@gmail.com>
2023-08-22 02:30:37 -0700

    add files

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { Home, CreatePost } from "./pages" 
+import logo from "./assets/Logo.png"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white 
+                        sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt="" srcset="" 
+            className='w-18 object-contain' />
+        </Link>
+        <Link to="/create-post"
+        className='font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+          Create
+        </Link>
+      </header>
+      <main>
+        
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
